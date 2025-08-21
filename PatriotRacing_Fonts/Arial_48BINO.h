9ac713f50b60a683ff1c9d41344f6072e48f47a8
Patriot Racing <63571895+BJPatriotRacing@users.noreply.github.com>
2025-08-21 10:40:33 -0500

    Add files via upload

---
+++ PatriotRacing_Fonts/Arial_48BINO.h
@@ -0,0 +1,16 @@
+#ifndef __Arial_48BINO_h__
+#define __Arial_48BINO_h__
+
+#include <ILI9341_t3.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const ILI9341_t3_font_t Arial_48BINO;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif
