24870a0d6c709f0f122fa42a5a3ddbf094e4e5c9
Patriot Racing <63571895+BJPatriotRacing@users.noreply.github.com>
2026-06-08 18:00:22 -0500

    Add files via upload

---
+++ PatriotRacing_Fonts/Arial_100_BINO.h
@@ -0,0 +1,16 @@
+#ifndef __Arial_100_BINO_h__
+#define __Arial_100_BINO_h__
+
+#include <ILI9341_t3.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const ILI9341_t3_font_t Arial_100_BINO;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif
